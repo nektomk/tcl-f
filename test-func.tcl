ecabff3ce95852051dd42c8b7c8ca11671c03e96
nektomk <nektomk@gmail.com>
2014-10-29 14:52:58 +0300

    delete test.tcl

--- test-func.tcl
+++
@@ -1,8 +0,0 @@
-lappend auto_path [pwd]
-package require f
-namespace import ::f::*
-
-set mid [ func {x y} {expr ($x + $y)/2.0} ]
-set add2 [ func x { expr $x+1 } ]
-set ret [ chain [ list $mid $add2 ] 23 17 ]
-puts $ret
